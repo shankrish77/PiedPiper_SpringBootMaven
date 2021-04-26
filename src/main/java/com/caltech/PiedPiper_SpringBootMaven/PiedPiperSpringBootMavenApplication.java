51376dd25a12083659474492e28d190a3523993e
shankrish77 <74517850+shankrish77@users.noreply.github.com>
2021-04-26 12:22:33 -0700

    Update PiedPiperSpringBootMavenApplication.java

--- src/main/java/com/caltech/PiedPiper_SpringBootMaven/PiedPiperSpringBootMavenApplication.java
+++ src/main/java/com/caltech/PiedPiper_SpringBootMaven/PiedPiperSpringBootMavenApplication.java
@@ -2,6 +2,9 @@ package com.caltech.PiedPiper_SpringBootMaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PiedPiperSpringBootMavenApplication {
@@ -11,3 +14,14 @@ public class PiedPiperSpringBootMavenApplication {
 	}
 
 }
+
+@RestController
+class GreetingController {
+    
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+}
+
+
